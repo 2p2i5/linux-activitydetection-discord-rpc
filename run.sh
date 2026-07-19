c6ca423282f29eb3c9703a8dc5630221dfc11129
2p2i5 <2ps2eq5@gmail.com>
2026-07-19 16:12:08 -0500

    update runsh to automatically update detectables

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+echo "updating detectables, make sure updatedetectables.sh is executable"
+echo " "
+
+./updatedetectables.sh
+
+echo " "
+echo "running program"
+echo " "
+
 source ./venv/bin/activate && python3 main.py
--- updatedetectables.sh
+++ updatedetectables.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 echo "getting list"
+echo " "
 
 curl https://discord.com/api/v10/games/detectable -o detectable.json 
 
+echo " "
 echo "done"
 
 sleep 3
